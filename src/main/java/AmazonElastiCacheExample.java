53db853cfc3a4376205687d5b3732879fe505692
wangbo <bjutwangboy@aliyun.com>
2018-06-21 19:48:29 +0800

    add jedis cluster

--- src/main/java/AmazonElastiCacheExample.java
+++ src/main/java/AmazonElastiCacheExample.java
@@ -5,9 +5,15 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.elasticache.AmazonElastiCacheClient;
 import com.amazonaws.services.elasticache.model.*;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This is a simple program to work with AmazonElastiCache on AWS.
@@ -39,7 +45,7 @@ public class AmazonElastiCacheExample {
         System.out.println("Got client, client.getEndpointPrefix() = " + client.getEndpointPrefix());
         client.setRegion(Region.getRegion(Regions.AP_NORTHEAST_1));
 //		client.setRegion(Region.getRegion(Regions.EU_CENTRAL_1));
-    //		client.setEndpoint("https://hermes-dev-0001-001.nquffl.0001.apn2.cache.amazonaws.com:6379");
+        //		client.setEndpoint("https://hermes-dev-0001-001.nquffl.0001.apn2.cache.amazonaws.com:6379");
         System.out.println("setEndpoint passed.");
         DescribeCacheClustersRequest dccRequest = new DescribeCacheClustersRequest();
         dccRequest.setShowCacheNodeInfo(true);
@@ -52,6 +58,8 @@ public class AmazonElastiCacheExample {
 
         List<CacheCluster> cacheClusters = clusterResult.getCacheClusters();
         System.out.println("About to enter for loop now, cacheClusters.size() = " + cacheClusters.size());
+        Set<HostAndPort> jedisClusterNode = new HashSet<HostAndPort>();
+
         for (CacheCluster cacheCluster : cacheClusters) {
             System.out.println("In for loop now.");
             for (CacheNode cacheNode : cacheCluster.getCacheNodes()) {
@@ -60,10 +68,13 @@ public class AmazonElastiCacheExample {
                 String addr = cacheNode.getEndpoint().getAddress();
 //				if (!addr.startsWith("hermes-dev")) continue;
                 int port = cacheNode.getEndpoint().getPort();
-                String url =  addr + ":" + port;
+                String url = addr + ":" + port;
                 System.out.println("formed url is: " + url);
 
+                jedisClusterNode.add(new HostAndPort(addr, port));
+
                 Jedis jedis = new Jedis(url);
+                jedis.getClient().setPassword("Adddeuuria88888888");
                 System.out.println("Connection to server sucessfully");
                 // check whether server is running or not
                 System.out.println("Server is running: " + jedis.ping());
@@ -71,5 +82,18 @@ public class AmazonElastiCacheExample {
             }
         }
 
+///////////////////////
+        JedisPoolConfig config = new JedisPoolConfig();
+        // 最大连接数
+        config.setMaxTotal(30);
+        // 最大连接空闲数
+        config.setMaxIdle(2);
+        //集群结点
+        //        JedisCluster jc = new JedisCluster(jedisClusterNode, config);
+        JedisCluster jcd = new JedisCluster(jedisClusterNode, 5, 5,
+                5, "Adddeuuria88888888", config);
+        jcd.set("name", "zhangsan");
+        String value = jcd.get("name");
+        System.out.println(value);
     }
 }
